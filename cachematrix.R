3b415b0078ec3108df046c3f97509efbaeb8e0a6
Bob <github@golios.com>
2019-12-28 12:01:18 +0800

    Tested both functions

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  cachedInverse <- NULL
+  set <- function(y){
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) cachedInverse <<- inverse
+  getinverse <- function() cachedInverse
+  list (set = set, get = get, 
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
@@ -12,4 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getinverse()
+  if (!is.null(inverse)){
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  message("Performing a solve")
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
